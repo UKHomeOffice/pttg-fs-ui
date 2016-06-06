5dc59c4c2c6fa9f42cef1735b6ccc9f8f93f8d48
atodd <alistair.todd@digital.homeoffice.gov.uk>
2016-06-06 10:56:51 +0100

    Empty Spring Boot app

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-pttg-financial-status-service-ui
+Financial Status Service UI
+=
+
+Overview
+-
+
+This is an overview of the financial status service UI.
+The financial status service UI is a UI for the financial status service.
+That was an overview of the financial status service UI.
---
+++ src/main/java/uk/gov/digital/ho/proving/financial/ServiceRunner.java
@@ -0,0 +1,26 @@
+package uk.gov.digital.ho.proving.financial;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class ServiceRunner {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(ServiceRunner.class);
+
+    //@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ServiceRunner.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        LOGGER.debug("starting pttg-financial-status-service-ui");
+
+        run(ServiceRunner.class, args);
+    }
+
+}
