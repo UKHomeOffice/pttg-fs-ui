5ab76d504c8ca46820fa2e9cee28dae991a50c23
atodd <alistair.todd@digital.homeoffice.gov.uk>
2016-06-07 17:42:55 +0100

    Playing around with spring boot auditing

--- src/main/java/uk/gov/digital/ho/proving/financial/domain/LoginListener.java
+++ src/main/java/uk/gov/digital/ho/proving/financial/domain/LoginListener.java
@@ -1,7 +1,22 @@
 package uk.gov.digital.ho.proving.financial.domain;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.actuate.audit.listener.AuditApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
 /**
  * @Author Home Office Digital
  */
-public class LoginListener {
+@Component
+public class LoginListener implements ApplicationListener<AuditApplicationEvent> {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(LoginListener.class);
+
+    @Override
+    public void onApplicationEvent(AuditApplicationEvent event) {
+
+        LOGGER.debug("audit event: {}", event.getAuditEvent().toString());
+    }
 }
