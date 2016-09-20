ac1abfa5f612dce65b275869fef27f807c531896
Andy Birkett <andrew.birkett@digital.homeoffice.gov.uk>
2016-09-20 11:07:51 +0100

    Add JAVA_OPTS to command line to limit memory used by spring boot

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 NAME=${NAME:-pttg-fs-ui}
 
 JAR=$(find . -name ${NAME}*.jar|head -1)
-java -Dcom.sun.management.jmxremote.local.only=false -Djava.security.egd=file:/dev/./urandom -jar "${JAR}"
+java ${JAVA_OPTS}  -Dcom.sun.management.jmxremote.local.only=false -Djava.security.egd=file:/dev/./urandom -jar "${JAR}"
