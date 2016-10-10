ba40d0d6ef3eabd940794a7217bea84f795acad6
Andy Birkett <andrew.birkett@digital.homeoffice.gov.uk>
2016-10-10 15:08:36 +0100

    Update build process and remove bower fragments

--- ci_build.sh
+++ ci_build.sh
@@ -8,29 +8,23 @@ GIT_COMMIT=${GIT_COMMIT:0:7}
 VERSION=$(grep ^version build.gradle | cut -d= -f 2 | tr -d ' ' | sed -e "s|'||g" | sed -e "s|version|v|g")
 
 build_image_for_app_build() {
-  docker build -t "${GRADLE_IMAGE}" -f Dockerfile.build .
+  docker build --no-cache -t "${GRADLE_IMAGE}" -f Dockerfile.build .
 }
 
 build_app() {
-  local MOUNT="${PWD}:/code"
-
-  # Mount in the local gradle cache into the docker container
-  [ -d "${HOME}/.gradle/caches" ] && MOUNT="${MOUNT} -v ${HOME}/.gradle/caches:/root/.gradle/caches"
-
-  # Mount in local maven repository into the docker container
-  [ -d "${HOME}/.m2/repository" ] && MOUNT="${MOUNT} -v ${HOME}/.m2/repository:/root/.m2/repository"
-
-  # Mount in local gradle user directory
-  [ -d "${HOME}/.gradle" ] && MOUNT="${MOUNT} -v ${HOME}/.gradle:/root/.gradle"
-
   ENV_OPTS="GIT_COMMIT=${GIT_COMMIT} -e VERSION=${VERSION}"
   [ -n "${BUILD_NUMBER}" ] && ENV_OPTS="BUILD_NUMBER=${BUILD_NUMBER} -e ${ENV_OPTS}"
 
-  docker run -e ${ENV_OPTS} -v ${MOUNT} "${GRADLE_IMAGE}" "${@}"
+  oldContainers=$(docker ps -a -q -f status=exited)
+  if [[ $oldContainers ]]; then
+    docker rm -v $oldContainers
+  fi
+  docker run --name pttg-fs-ui-build -e ${ENV_OPTS}  "${GRADLE_IMAGE}" "${@}"
+  mkdir -p build/libs
+  docker cp pttg-fs-ui-build:/work/build/libs/pttg-fs-ui-${VERSION}.${GIT_COMMIT}.jar build/libs/
 }
 
 set_props() {
-  [ -n "${BUILD_NUMBER}" ] && VERSION="${VERSION}-${BUILD_NUMBER}"
   [ -n "${GIT_COMMIT}" ] && VERSION="$VERSION.${GIT_COMMIT}"
   echo "VERSION=${VERSION}" > version.properties
 }
@@ -80,7 +74,7 @@ main() {
   echo "=== dockerBuild function"
   build_image_that_runs_app
 
-  echo "=== dockerPublish function - temp disabled"
+  echo "=== dockerPublish function"
   publish
 }
 
