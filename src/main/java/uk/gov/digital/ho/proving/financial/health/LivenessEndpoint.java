6feff0b431564fe0955fb1d5c63ed45cfb5ecaee
Alistair Todd <alistair.todd@digital.homeoffice.gov.uk>
2016-11-03 15:41:49 +0000

    Add liveness endpoint

--- src/main/java/uk/gov/digital/ho/proving/financial/health/LivenessEndpoint.java
+++ src/main/java/uk/gov/digital/ho/proving/financial/health/LivenessEndpoint.java
@@ -3,5 +3,22 @@ package uk.gov.digital.ho.proving.financial.health;
 /**
  * Created by Home Office Digital.
  */
-public class LivenessEndpoint {
+
+import org.springframework.boot.actuate.endpoint.AbstractEndpoint;
+import org.springframework.stereotype.Component;
+
+/**
+ * Endpoint that can be used for liveness checks, but not for Readiness.
+ */
+@Component
+public class LivenessEndpoint extends AbstractEndpoint<String> {
+
+    public LivenessEndpoint() {
+        super("ping");
+    }
+
+    @Override
+    public String invoke() {
+        return "pong";
+    }
 }
