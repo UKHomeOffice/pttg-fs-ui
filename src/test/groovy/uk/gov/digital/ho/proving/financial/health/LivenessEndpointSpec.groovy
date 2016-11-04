89217df5e6e82521c47e72c5d47708995408e5c1
Alistair Todd <alistair.todd@digital.homeoffice.gov.uk>
2016-11-04 09:59:38 +0000

    Cache settings for healthcheck

--- src/test/groovy/uk/gov/digital/ho/proving/financial/health/LivenessEndpointSpec.groovy
+++ src/test/groovy/uk/gov/digital/ho/proving/financial/health/LivenessEndpointSpec.groovy
@@ -1,7 +1,21 @@
 package uk.gov.digital.ho.proving.financial.health
 
+import spock.lang.Specification
+
 /**
  * Created by Home Office Digital.
  */
-class LivenessEndpointSpec {
+class LivenessEndpointSpec extends Specification {
+
+    def "should answer with pong"() {
+
+        expect:
+        new LivenessEndpoint().invoke().equalsIgnoreCase("pong")
+    }
+
+    def "should be called ping" () {
+
+        expect:
+        new LivenessEndpoint().getId().equalsIgnoreCase("ping")
+    }
 }
