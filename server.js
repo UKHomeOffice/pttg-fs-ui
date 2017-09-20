f4e5b09b963e9d5d9a99f3418d1618a594179aa3
Andy Marke <andy.marke@digital.homeoffice.gov.uk>
2017-09-20 08:47:13 +0100

    PTTG-387 bring in line with ip (#20)
    * PTTG-387 Added server certificates and useful headers for logging
    
    * PTTG-387 Added server certificates and useful headers for logging

--- server.js
+++ server.js
@@ -7,13 +7,40 @@ var apiBaseUrl = apiRoot + '/pttg/financialstatus/v1/'
 var request = require('request')
 var port = process.env.SERVER_PORT || '8000'
 var moment = require('moment')
+var uuid = require('uuid/v4')
 
 // required when running BDDs to force to root directory
 var path = require('path')
 process.chdir(path.resolve(__dirname))
 
-var stdRelay = function (res, uri, qs) {
-  request({uri: uri, qs: qs}, function (error, response, body) {
+var addSecureHeaders = function (res) {
+    res.setHeader('Cache-control', 'no-store, no-cache')
+}
+
+
+var stdRelay = function (req, res, uri, qs) {
+  var headers = {}
+
+  addSecureHeaders(res)
+
+  if (req.headers['x-auth-userid']) {
+    headers['x-auth-userid'] = req.headers['x-auth-userid']
+  }
+
+  if (req.headers['kc-access']) {
+    headers['kc-access'] = req.headers['kc-access']
+  }
+
+  headers['x-correlation-id'] = uuid()
+
+  var opts = {
+    uri: uri,
+    qs: qs,
+    headers: headers
+  }
+  opts = addCaCertsForHttps(opts, headers)
+
+  request(opts, function (error, response, body) {
     var status = (response && response.statusCode) ? response.statusCode : 500
     if ((body === '' || body === '""') && status === 200) {
       status = 500
@@ -55,15 +82,15 @@ app.get('/healthz', function (req, res) {
 })
 
 app.get(uiBaseUrl + 'availability', function (req, res) {
-  stdRelay(res, apiRoot + '/healthz', '')
+  stdRelay(req, res, apiRoot + '/healthz', '')
 })
 
 app.get(uiBaseUrl + ':tier/threshold', function (req, res) {
-  stdRelay(res, apiBaseUrl + req.params.tier + '/maintenance/threshold', req.query)
+  stdRelay(req, res, apiBaseUrl + req.params.tier + '/maintenance/threshold', req.query)
 })
 
 app.get(uiBaseUrl + 'accounts/:sortCode/:accountNumber/consent', function (req, res) {
-  stdRelay(res, apiBaseUrl + 'accounts/' + req.params.sortCode + '/' + req.params.accountNumber + '/consent', req.query)
+  stdRelay(req, res, apiBaseUrl + 'accounts/' + req.params.sortCode + '/' + req.params.accountNumber + '/consent', req.query)
 })
 
 app.get(uiBaseUrl + ':tier/accounts/:sortCode/:accountNumber/dailybalancestatus', function (req, res) {
@@ -78,7 +105,7 @@ app.get(uiBaseUrl + ':tier/accounts/:sortCode/:accountNumber/dailybalancestatus'
     try {
       req.query.minimum = JSON.parse(body).threshold
       req.query.fromDate = moment(req.query.endDate).subtract(getDaysToCheck(req.params.tier) - 1, 'd').format('YYYY-MM-DD')
-      stdRelay(res, apiBaseUrl + 'accounts/' + req.params.sortCode + '/' + req.params.accountNumber + '/dailybalancestatus', req.query)
+      stdRelay(req, res, apiBaseUrl + 'accounts/' + req.params.sortCode + '/' + req.params.accountNumber + '/dailybalancestatus', req.query)
     } catch (e) {
       // console.log('ERROR:')
       // console.log(e)
@@ -90,5 +117,18 @@ app.get(uiBaseUrl + ':tier/accounts/:sortCode/:accountNumber/dailybalancestatus'
 })
 
 app.get(uiBaseUrl + ':tier/conditioncodes', function (req, res) {
-  stdRelay(res, apiBaseUrl + req.params.tier + '/conditioncodes', req.query)
+  stdRelay(req, res, apiBaseUrl + req.params.tier + '/conditioncodes', req.query)
 })
+
+function addCaCertsForHttps (opts, headers) {
+    // log("About to call " + opts.uri, headers)
+    if (opts.uri && opts.uri.toLowerCase().startsWith('https')) {
+        // log("Loading certs from  " + process.env.CA_CERTS_PATH, headers)
+        opts.ca = fs.readFileSync(process.env.CA_CERTS_PATH, 'utf8')
+        // DSP certs do not include root ca - so we can not validate entire chain that OpenSSL requires
+        // so until we have entire chain in bundle lets not be strict
+        opts.strictSSL = false
+    }
+    return opts
+}
+
