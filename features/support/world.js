188d5bcc7d0dc06189aa78cebf9a186e41f6641c
Stuart Craigen <s.craigen@mac.com>
2018-06-12 11:20:36 +0100

    Add ability to toggle headless using env var, useful for local dev

--- features/support/world.js
+++ features/support/world.js
@@ -16,7 +16,7 @@ var reportPath = path.resolve('report/')
 // config
 var shareBrowserInstances = true
 var browserName = 'chrome'
-var headless = true
+var headless = (process.env.HEADLESS !== false && process.env.HEADLESS !== 'false')
 var showReport = false
 //
 
